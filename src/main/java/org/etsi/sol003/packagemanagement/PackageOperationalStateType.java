988f4b24c6013fc2c6ae1a5adb360b37c5081bd3
Chris <chris.lloyd@accantosystems.com>
2019-09-20 10:56:18 +0100

    Added Package Management model classes

---
+++ src/main/java/org/etsi/sol003/packagemanagement/PackageOperationalStateType.java
@@ -0,0 +1,18 @@
+package org.etsi.sol003.packagemanagement;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * Operational state of the on-boarded instance of the VNF package.
+ */
+public enum PackageOperationalStateType {
+    /**
+     * The VNF package is enabled, i.e. it can be used for instantiation of new VNF instances.
+     */
+    ENABLED,
+    /**
+     * The VNF package is disabled, i.e. it cannot be used for further VNF instantiation requests (unless and until the VNF package is re-enabled).
+     */
+    DISABLED;
+
+}
