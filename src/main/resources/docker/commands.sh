0c6fe4cf7d285cae2d668246eb564e4010fe3bfd
Chris Lloyd <chris.lloyd1@ibm.com>
2021-03-11 10:30:12 +0000

    issues/15 - allow certs certificate to be configured

--- src/main/resources/docker/commands.sh
+++ src/main/resources/docker/commands.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+mkdir -p /var/lm/truststore/
+TRUSTSTORE=/var/lm/truststore/truststore.p12
+# Add certificates to the truststore
+for cert in `find /var/lm -name *.cer -type l`; do
+  echo "$(date) Importing certificate ${cert} into ${TRUSTSTORE}"
+  $JAVA_HOME/bin/keytool -import -alias $(basename ${cert} .cer) -file ${cert} -keystore ${TRUSTSTORE} -storepass changeit -noprompt
+  TS_OPTION=-Djavax.net.ssl.trustStore=${TRUSTSTORE}
+done
+
+
 cd ${alm_vnfmdriver_directory}
-java $JVM_OPTIONS -jar /data/sol003-lifecycle-driver-@project.version@.jar
+java $TS_OPTION $JVM_OPTIONS -jar /data/sol003-lifecycle-driver-@project.version@.jar
