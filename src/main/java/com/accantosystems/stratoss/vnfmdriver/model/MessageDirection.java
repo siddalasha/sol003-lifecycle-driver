f2e9a06e89efc3321a42a13c37987805a238fbd0
rkumar8j <rkumar8j@in.ibm.com>
2022-08-08 14:48:44 +0530

    Changes as per review comments by Loka
    Signed-off-by: rkumar8j <rkumar8j@in.ibm.com>

--- src/main/java/com/accantosystems/stratoss/vnfmdriver/model/MessageDirection.java
+++ src/main/java/com/accantosystems/stratoss/vnfmdriver/model/MessageDirection.java
@@ -1,9 +1,6 @@
 package com.accantosystems.stratoss.vnfmdriver.model;
 
 public enum MessageDirection {
-
-    RECEIVED("received"), SENT("sent");
-
-    MessageDirection(String sent) {
-    }
+    received,
+    sent
 }
--- src/main/java/com/accantosystems/stratoss/vnfmdriver/model/MessageType.java
+++ src/main/java/com/accantosystems/stratoss/vnfmdriver/model/MessageType.java
@@ -1,9 +1,7 @@
 package com.accantosystems.stratoss.vnfmdriver.model;
 
 public enum MessageType {
-
-    REQUEST("request"), RESPONSE("response"), MESSAGE("message");
-
-    MessageType(String request) {
-    }
+    request,
+    response,
+    message
 }
