99b0a888139e8ccc981da0768df7119df0bcfd41
Guduru-Siddalasha <Guduru.Siddalasha@ibm.com>
2022-11-10 00:48:16 -0800

    changes in swagger

--- src/main/java/com/accantosystems/stratoss/vnfmdriver/config/SwaggerConfiguration.java
+++ src/main/java/com/accantosystems/stratoss/vnfmdriver/config/SwaggerConfiguration.java
@@ -1,18 +1,14 @@
 package com.accantosystems.stratoss.vnfmdriver.config;
 
-import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
-/**
- * Springfox Swagger configuration.
- * <p>
- * Warning! When having a lot of REST endpoints, Springfox can become a performance issue. In that case, you can use a
- * specific Spring profile for this class, so that only front-end developers have access to the Swagger view.
- */
+
 @Configuration
 public class SwaggerConfiguration {
 
@@ -28,21 +24,16 @@ public class SwaggerConfiguration {
     @Value("${info.contact.name:Accanto Systems Ltd}")
     private String contactName;
 
-    //@Value("${swagger.server-url}")
+   // @Value("${swagger.server-url}")
     private String serverUrl;
 
-    /**
-     * Swagger Springfox configuration.
-     *
-     * @return the Swagger Springfox configuration
-     */
     @Bean
-    public OpenApiCustomiser openApiCustomiser() {
-        return openApi -> {
-            openApi.getPaths().values().forEach(pathItem ->
-            pathItem.readOperations());;
-        };
-    }
-        
+  public OpenAPI springShopOpenAPI() {
+      return new OpenAPI()
+              .info(new Info().title(appName)
+              .description(appDescription)
+              .version(appVersion));
+              
+  }
 }
     
